8f24b6217eecc217834a6b0d15766e3791fe3de3
jivko25 <jivkou@yahoo.com>
2023-09-18 16:01:30 +0300

    feat: create express app and wire it with docker

---
+++ app.js
@@ -0,0 +1,18 @@
+// Import the express module
+let express = require("express");
+
+// Create an express application
+let server = express();
+
+// Define the port to be used
+let listeningPort = 3000;
+
+// Define route for the root URL
+server.get("/", (request, response) => {
+  response.send("My First Express JS Application");
+});
+
+// Start the application
+server.listen(listeningPort, () => {
+  console.log(`Server is up and running at http://localhost:${listeningPort}`);
+});
